bba31018d5039ce83712b2ecea2df7da94d0a10f
Markus Klepping <markus.klepping@code.berlin>
2022-05-02 16:38:36 +0200

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+from os import environ
+
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL').replace('postgres://', 'postgresql://', 1)
+
+SECRET_KEY = environ.get('SECRET_KEY')
